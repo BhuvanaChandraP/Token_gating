318191bafa3bf8ef946f7a3933ac2f20bdc3e112
Shruti0316 <shruti16.k@gmail.com>
2022-10-15 20:05:50 +0530

    feat: landing page

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -16,14 +16,19 @@ const LandingPage = () => {
     };
   return (
     <div className="d-flex flex-column justify-content-center align-items-center h-100">
-   
-    
-        <button onClick={onBuyToken} className="btn btn-primary btn-lg">
-       
-            {loading ? "Loading..." : "Buy Token"}
-        </button>
-    
-  
+      <div className="p-2">
+        <h1 className="display-1" style={{fontWeight: "350"}}>NFT Gated Token </h1> 
+      </div>
+      <p className="lead">
+        Serve exclusive content to users who own an NFT.
+      </p>
+      <p className="lead">
+      To access the main page buy a token from our collection!
+      </p>
+      <br/><br/>
+      <button onClick={onBuyToken} className="btn btn-primary btn-lg">
+        {loading ? "Loading..." : "Buy Token"}
+      </button>
     </div>
   );
 };
